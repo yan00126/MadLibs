1c93c19a3ca199967b64d4b8bb521cf2d8545a57
Michael Eisenbraun <eisenbm@algonquincollege.com>
2024-07-19 07:28:40 -0400

    Creates starter files

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# MadLibs
-This repository contains the starter files for the Mad Libs project
+# Mad Libs
+
+## Objective
+Mad Libs is a fun word game where players provide types of words (nouns, verbs, adjectives, etc. ) without knowing the context in which the words will be used. Those words are then placed inside a story, often with hilarious results.
+
+Create an interactive game of Mad Libs using Blazor.
---
+++ Validation/NotEqualAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace MadLibs;
+
+public class NotEqualAttribute : ValidationAttribute
+{
+    private readonly string otherProperty;
+
+    public NotEqualAttribute(string otherProperty)
+    {
+        this.otherProperty = otherProperty;
+    }
+
+    protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+    {
+        var otherPropertyInfo = validationContext.ObjectType.GetProperty(otherProperty);
+        if (otherPropertyInfo == null)
+        {
+            return new ValidationResult($"Property with name {otherProperty} not found.");
+        }
+
+        var otherPropertyValue = otherPropertyInfo.GetValue(validationContext.ObjectInstance);
+        if (Equals(value, otherPropertyValue))
+        {
+            if (ErrorMessage != null)
+            {
+                return new ValidationResult(ErrorMessage);
+            }
+            
+            return new ValidationResult($"{validationContext.DisplayName} should not be equal to {otherProperty}.");
+        }
+
+        return ValidationResult.Success!;
+    }
+}
+
